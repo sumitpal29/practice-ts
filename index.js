00488e4cf935f8234503afd9e7429b329db4c6a1
sumitpal.29 <sumitwithyou@gmail.com>
2023-09-15 18:05:14 +0530

    typescript initialised

---
+++ index.js
@@ -0,0 +1,4 @@
+function sumTwoNumbers(a, b) {
+    return a + b;
+}
+console.log(sumTwoNumbers(4, "asaa"));
---
+++ index.ts
@@ -0,0 +1,4 @@
+function sumTwoNumbers(a: number, b: number): number {
+  return a + b;
+}
+console.log(sumTwoNumbers(4, "asaa"));
